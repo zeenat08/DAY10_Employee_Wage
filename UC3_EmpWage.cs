3497bbddc6f8b3b76ea4ce44cfac10485f684a9b
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-23 14:50:30 +0530

    ZEENAT: added Employee part time wage modified file

--- UC3_EmpWage.cs
+++ UC3_EmpWage.cs
@@ -24,7 +24,7 @@ namespace day4
             //else
             //empHrs = 0;
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Emp wage: " + empWage);
             Console.ReadKey();
         }
     }
